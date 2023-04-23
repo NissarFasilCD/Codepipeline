3b461542c298802f5a9ee6d8db1c385dbb1c08fa
NissarFasilCD <nissarfasil.khan@crystaldelta.com>
2023-04-23 20:40:05 +0530

    add

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
+'use strict';
+
 const express = require('express');
+
+// Constants
+const PORT = 9000;
+const HOST = 9000;
+
+// App
 const app = express();
-const port = 9000;
-
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  res.send('<h1 style="color:green;">Java Home App - version-10!!</h1> \n');
 });
 
-app.listen(port, () => {
-  console.log(`App listening on port ${port}`);
-});
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
