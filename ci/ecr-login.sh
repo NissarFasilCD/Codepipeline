8ecbc6016fa9c6d75320d2a804f7873ac9e79263
NissarFasilCD <nissarfasil.khan@crystaldelta.com>
2023-04-24 11:52:19 +0530

    add

--- ci/ecr-login.sh
+++ ci/ecr-login.sh
@@ -1,21 +1,23 @@
-ROLE_ARN=$1
-IMAGE_PATH=$2
-ROLE_SESSION_NAME=code-pipeline
+# ROLE_ARN=$1
+# IMAGE_PATH=$2
+# ROLE_SESSION_NAME=code-pipeline
 
-echo $ROLE_ARN
-echo $IMAGE_PATH
-# this assumes that the ecr_login has been done before which gives the docker container access to the production server deployment role
-CREDENTIALS=`aws sts assume-role --role-arn $ROLE_ARN --role-session-name $ROLE_SESSION_NAME`
+# echo $ROLE_ARN
+# echo $IMAGE_PATH
+# # this assumes that the ecr_login has been done before which gives the docker container access to the production server deployment role
+# CREDENTIALS=`aws sts assume-role --role-arn $ROLE_ARN --role-session-name $ROLE_SESSION_NAME`
 
-export AWS_ACCESS_KEY_ID=`echo $CREDENTIALS | jq -r .Credentials.AccessKeyId`
-export AWS_SECRET_ACCESS_KEY=`echo $CREDENTIALS | jq -r .Credentials.SecretAccessKey`
-export AWS_SESSION_TOKEN=`echo $CREDENTIALS | jq -r .Credentials.SessionToken`
+# export AWS_ACCESS_KEY_ID=`echo $CREDENTIALS | jq -r .Credentials.AccessKeyId`
+# export AWS_SECRET_ACCESS_KEY=`echo $CREDENTIALS | jq -r .Credentials.SecretAccessKey`
+# export AWS_SESSION_TOKEN=`echo $CREDENTIALS | jq -r .Credentials.SessionToken`
 
-export AWS_DEFAULT_REGION='ap-southeast-2'
-echo $AWS_ACCESS_KEY_ID
-echo $AWS_SECRET_ACCESS_KEY
-echo $AWS_SESSION_TOKEN
+# export AWS_DEFAULT_REGION='ap-southeast-2'
+# echo $AWS_ACCESS_KEY_ID
+# echo $AWS_SECRET_ACCESS_KEY
+# echo $AWS_SESSION_TOKEN
 
-# now do the login
-# eval sudo su -
-login_string=`aws ecr get-login-password --region ap-southeast-2 | docker login --username AWS --password-stdin $IMAGE_PATH`;
+# # now do the login
+# # eval sudo su -
+# login_string=`aws ecr get-login-password --region ap-southeast-2 | docker login --username AWS --password-stdin $IMAGE_PATH`;
+
+echo "hello world"
